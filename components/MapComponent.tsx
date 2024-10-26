c608e3baafb14035a5f004554fc8abfc59cf9ad3
YetaGeta <me@yetageta.com>
2024-10-26 22:12:00 +0100

    map

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,5 +1,5 @@
 import { Text, View, TextInput, Button } from "react-native";
-import { Canvas, Path, Skia, DashPathEffect } from "@shopify/react-native-skia";
+import { Canvas, Path, Skia, DashPathEffect, useImage, Image } from "@shopify/react-native-skia";
 import {
     useSharedValue,
     withTiming,
@@ -18,15 +18,17 @@ function translateMapCoordinatesToScreen(x: number, y: number, imageWidth: numbe
     return {x: x/imageWidth * canvasWidth, y: y/imageHeight * canvasHeight}
 }
 
+
 export default function MapComponent() {
     const offset = useSharedValue(0);
 
+    const image = useImage(require("../assets/images/tempMap.png"))
+
+
     const path = Skia.Path.Make();
-    path.moveTo(0, 0);
-    path.lineTo(400, 0)
-    path.lineTo(400, 400)
-    path.lineTo(0, 400)
-    path.close();
+    path.moveTo(200, 200);
+    path.lineTo(200, 300)
+    path.lineTo(0, 300)
 
     useEffect(() => {
         offset.value = withRepeat(
@@ -38,6 +40,7 @@ export default function MapComponent() {
      
     return (
         <Canvas style={{ width: 400, height: 400 }}>
+            <Image image={image} fit="fill" x={0} y={0} width={400} height={400} />
             <Path
                 path={path}
                 style={"stroke"}
