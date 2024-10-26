ece9da90dfb880a9ca08dd73a4ce7652a36e3b07
YetaGeta <me@yetageta.com>
2024-10-26 14:44:29 +0100

    animated path - run setup cmd

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,7 +1,16 @@
 import { Text, View, TextInput, Button } from "react-native";
 import { Canvas, Path, Skia, DashPathEffect } from "@shopify/react-native-skia";
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withTiming,
+    withRepeat,
+  } from 'react-native-reanimated';  
+import { useEffect } from "react";
 
 export default function MapComponent() {
+    const offset = useSharedValue(0);
+
     const path = Skia.Path.Make();
     path.moveTo(128, 0);
     path.lineTo(168, 80);
@@ -15,6 +24,14 @@ export default function MapComponent() {
     path.lineTo(88, 80);
     path.lineTo(128, 0);
     path.close();
+
+    useEffect(() => {
+        offset.value = withRepeat(
+            withTiming(1, { duration: 3000 }),
+            -1,
+            false
+        );
+    }, [])
      
     return (
         <Canvas style={{ width: 400, height: 600 }}>
@@ -23,9 +40,8 @@ export default function MapComponent() {
                 style={"stroke"}
                 strokeWidth={3}
                 color="gray"
-                start={0.05} // use this to show the direction the path is going in
+                start={offset} // use this to show the direction the path is going in
             >
-                <DashPathEffect intervals={[4,4]} />
             </Path>
         </Canvas>
     );
