8c0b93cbd575d369f0c047f6017ce1b4210aee08
Jonathan Lee <jlee4889@gmail.com>
2024-10-26 15:33:27 +0100

    update shopping list page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import {ActivityIndicator, Button, FlatList, Text, View, ScrollView, TextInput, StyleSheet} from 'react-native';
 // import getRecipes from 'shop'; 
 
-type Movie = {
+type Food = {
   id: string;
   name: string;
   aisle: string;
+  price: number;
 };
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    height: 40,
+    marginBottom: 5
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  }
+})
 
 const App = () => {
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState<Movie[]>([]);
+  const [data, setData] = useState<Food[]>([]);
 
-  const getMovies = async () => {
+  const getFoods = async () => {
     try {
       const response = await fetch('data/test.json');
       const json = await response.json();
@@ -25,24 +39,33 @@ const App = () => {
   };
 
   useEffect(() => {
-    getMovies();
+    getFoods();
   }, []);
 
   return (
-    <View style={{flex: 1, padding: 24}}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({id}) => id}
-          renderItem={({item}) => (
-            <Text>
-              {item.name}, {item.aisle}
-            </Text>
-          )}
-        />
-      )}
+    <View style={styles.container}>
+      <View style={{flex: 1,
+          padding: 24, 
+          justifyContent: "center",
+          alignItems: "center"}}>
+        <TextInput style={styles.input} placeholder='Enter item'/>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({id}) => id}
+            renderItem={({item}) => (
+              <ScrollView style={{borderColor: "red",
+                borderWidth: 1,
+                maxWidth: 200
+              }}>
+                <Button title={ `${item.name}, £${item.price}` } color={"grey"}></Button>
+              </ScrollView>
+            )}
+          />
+        )}
+      </View>
     </View>
   );
 };
