129fe8b7ec0ceccf71683b87eb054c5858d04c8b
Abdulrahman Al Abri <abdulrahman.alabri@student.manchester.ac.uk>
2024-10-26 16:48:48 +0100

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, g, jsonify
-import sqlite3
+from flask import Flask, request, jsonify
 import pandas as pd
 from fuzzywuzzy import fuzz
 
