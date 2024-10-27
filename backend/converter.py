b5f9a89b22e7dfde02a1663c1df568cccdc4e1a7
Jonathan Lee <jlee4889@gmail.com>
2024-10-27 10:07:38 +0000

    update some files

--- backend/converter.py
+++
@@ -1,5 +0,0 @@
-with open("backend/coordinates.txt", "r") as f1:
-    for line in f1:
-        with open("backend/coordinates_doubled", "a") as f2:
-            f2.write(line)
-            f2.write(line)
